8a5293e8025ec4d22f19a45c23eeb7caf8c0a868
Takudzwa Titus Nyanhanga <takudzwa@takutitus.me>
2024-04-05 10:01:31 +0200

    Publish

---
+++ app/directors.js
@@ -0,0 +1,38 @@
+const directors = [
+    {
+        name: "Eleni Athiaki",
+        designation: "Founder | Artistic Designer",
+        imageUrl: "images/resource/team-1.jpg",
+        socialLinks: {
+          facebook: "#",
+          twitter: "#",
+          googlePlus: "#",
+          instagram: "#",
+          whatsapp: "#"
+        }
+      },{
+        name: "Audrey Tofara Nyoni",
+        designation: "Creative Director",
+        imageUrl: "images/resource/team-2.jpg",
+        socialLinks: {
+          facebook: "#",
+          twitter: "#",
+          googlePlus: "#",
+          instagram: "#",
+          whatsapp: "#"
+        }
+      },{
+        name: "Charles Shumba",
+        designation: "Technical Director",
+        imageUrl: "images/resource/team-3.jpg",
+        socialLinks: {
+          facebook: "#",
+          twitter: "#",
+          googlePlus: "#",
+          instagram: "#",
+          whatsapp: "#"
+        }
+      },
+    // Add other director objects here following the same structure.
+  ];
+  
---
+++ app/slides.js
@@ -0,0 +1,22 @@
+const slides = [
+    {
+        backgroundImage: "images/main-slider/background-1.jpg",
+        heading: "Architecture is a <br> Visual Art.",
+        text: "The buildings speak for themselves",
+        buttonLink: "about.html",
+        buttonText: "Check Art"
+      },{
+        backgroundImage: "images/main-slider/background-2.jpg",
+        heading: "Architecture is a <br> Visual Art.",
+        text: "The buildings speak for themselves",
+        buttonLink: "about.html",
+        buttonText: "Check Art"
+      },{
+        backgroundImage: "images/main-slider/background-3.jpg",
+        heading: "Architecture is a <br> Visual Art.",
+        text: "The buildings speak for themselves",
+        buttonLink: "about.html",
+        buttonText: "Check Art"
+      }
+    // Add more slide objects following the same structure.
+  ];
