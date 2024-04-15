7eee6f82da5df28ca070785fae9fcb3473c274f6
Takudzwa Titus Nyanhanga <takudzwa@takutitus.me>
2024-04-15 15:00:19 +0200

    Deploy

--- app/directors.js
+++ app/directors.js
@@ -1,38 +1,124 @@
 const directors = [
+  {
+      name: "Eleni Athiaki",
+      designation: "Founder | Artistic Designer",
+      imageUrl: "images/resource/team-1.jpg",
+      socialLinks: {
+        facebook: "#",
+        twitter: "#",
+        googlePlus: "#",
+        instagram: "#",
+        whatsapp: "#"
+      }
+    },{
+      name: "Audrey Tofara Nyoni",
+      designation: "Creative Director",
+      imageUrl: "images/resource/team-2.jpg",
+      socialLinks: {
+        facebook: "#",
+        twitter: "#",
+        googlePlus: "#",
+        instagram: "#",
+        whatsapp: "#"
+      }
+    },{
+      name: "Charles Shumba",
+      designation: "Technical Director",
+      imageUrl: "images/resource/team-3.jpg",
+      socialLinks: {
+        facebook: "#",
+        twitter: "#",
+        googlePlus: "#",
+        instagram: "#",
+        whatsapp: "#"
+      }
+    },
+  // Add other director objects here following the same structure.
+];
+const team = [
+  {
+      name: "Eleni Athiaki",
+      designation: "Founder | Artistic Designer",
+      imageUrl: "images/resource/team-1.jpg",
+      socialLinks: {
+        facebook: "#",
+        twitter: "#",
+        googlePlus: "#",
+        instagram: "#",
+        whatsapp: "#"
+      }
+    },
     {
-        name: "Eleni Athiaki",
-        designation: "Founder | Artistic Designer",
-        imageUrl: "images/resource/team-1.jpg",
-        socialLinks: {
-          facebook: "#",
-          twitter: "#",
-          googlePlus: "#",
-          instagram: "#",
-          whatsapp: "#"
-        }
-      },{
-        name: "Audrey Tofara Nyoni",
-        designation: "Creative Director",
-        imageUrl: "images/resource/team-2.jpg",
-        socialLinks: {
-          facebook: "#",
-          twitter: "#",
-          googlePlus: "#",
-          instagram: "#",
-          whatsapp: "#"
-        }
-      },{
-        name: "Charles Shumba",
-        designation: "Technical Director",
-        imageUrl: "images/resource/team-3.jpg",
-        socialLinks: {
-          facebook: "#",
-          twitter: "#",
-          googlePlus: "#",
-          instagram: "#",
-          whatsapp: "#"
-        }
-      },
-    // Add other director objects here following the same structure.
-  ];
+      name: "Audrey Tofara Nyoni",
+      designation: "Creative Director",
+      imageUrl: "images/resource/team-2.jpg",
+      socialLinks: {
+        facebook: "#",
+        twitter: "#",
+        googlePlus: "#",
+        instagram: "#",
+        whatsapp: "#"
+      }
+    },
+    {
+      name: "Charles Shumba",
+      designation: "Technical Director",
+      imageUrl: "images/resource/team-3.jpg",
+      socialLinks: {
+        facebook: "#",
+        twitter: "#",
+        googlePlus: "#",
+        instagram: "#",
+        whatsapp: "#"
+      }
+    },
+  // Add other director objects here following the same structure.
+  {
+    name: "Nyasha Yvette Chitambo",
+    designation: "Marketing Executive",
+    imageUrl: "images/resource/team-7.jpg",
+    socialLinks: {
+      facebook: "#",
+      twitter: "#",
+      googlePlus: "#",
+      instagram: "#",
+      whatsapp: "#"
+    }
+  },
+  {
+    name: "Tanaka Sibanda",
+    designation: "Interior Architect",
+    imageUrl: "images/resource/team-5.jpg",
+    socialLinks: {
+      facebook: "#",
+      twitter: "#",
+      googlePlus: "#",
+      instagram: "#",
+      whatsapp: "#"
+    }
+  },
+  {
+    name: "Nyasha Machita",
+    designation: "Interior Architect",
+    imageUrl: "images/resource/team-6.jpg",
+    socialLinks: {
+      facebook: "#",
+      twitter: "#",
+      googlePlus: "#",
+      instagram: "#",
+      whatsapp: "#"
+    }
+  },{
+  name: "Blessing Mabutu",
+  designation: "Designer",
+  imageUrl: "images/resource/team-8.jpg",
+  socialLinks: {
+    facebook: "#",
+    twitter: "#",
+    googlePlus: "#",
+    instagram: "#",
+    whatsapp: "#"
+  }
+}
   
+];
